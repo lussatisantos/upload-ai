36c03a8de96af044b2570c440fa859098f809fbc
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-20 17:12:33 +0100

    project started

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator"
 
 export function App() {
   return (
-  <Button>Teste</Button>
+  <div>
+
+    <div className="px-6 flex items-center justify-between border-b">
+      <h1 className="text-x1 font-bold">upload.ai</h1>
+      
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">Desenvolvido por Lussati Santos na NLW IA da Rocketseat</span>
+
+        <Separator orientation='vertical' className='h-6'/>
+        <Button variant="outline">
+          <Github className="m-4 h-4 mr-2" />
+          Github</Button>
+      </div>
+    </div>
+  </div>
   )
 }
