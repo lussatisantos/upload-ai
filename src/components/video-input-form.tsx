6e3ee334d12c208f6c66004c80a19c6734b47b02
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-24 17:19:17 +0100

    Update video-input-form.tsx

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,37 +3,56 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export function VideoInputForm() {
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
     function  handleFileSelection(event: ChangeEvent<HTMLInputElement>) {
-        const { files } = event.currentTarget
+      const { files } = event.currentTarget
 
         if(!files) {
-            return 
+            return
         }
-        
         const selectedFile = files[0]
-
+        
         setVideoFile(selectedFile)
+    
+      }
+    
+    function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault()
 
-        const previewURL = useMemo(() => {
-            if(!videoFile) {
-                return null
-            }
-            
-            return URL.createObjectURL(videoFile)
-        }, [videoFile])
+      const prompt = promptInputRef.current
+
+      if (!videoFile) {
+        return
+      }
+
+      // converter o video em audio
+      
     }
+
+    const previewURL = useMemo(() => {
+      if (!videoFile) {
+          return null
+      }
+            
+      return URL.createObjectURL(videoFile)
+    }, [videoFile])
+
     return (
-        <form className="space-y-6">
-        <label htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+        <form onSubmit={handleUploadVideo} className="space-y-6">
+        <label 
+          htmlFor="video"
+          className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
         >
-          { previewURL ? (
-            <video src={ previewURL } controls={false} className="pointer-events-none" />
+          {previewURL ? (
+            <video 
+            src={previewURL} 
+            controls={false} 
+            className="pointer-events-none absolute inset-0" />
           ) : (
             <>
             <FileVideo className="w-4 h-4" />
@@ -45,9 +64,11 @@ export function VideoInputForm() {
         <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelection} />
 
         <Separator />
+
         <div className="space-y-2">
           <Label htmlFor="transcription_prompt">Prompt de transcricao</Label>
-          <Textarea 
+          <Textarea
+            ref={promptInputRef}
             id="transcription__prompt" 
             className="h-20 leading-relaxed resize-none"
             placeholder="Inclua palavras-chave mencionadas no video separadas por virgula (,)"  
